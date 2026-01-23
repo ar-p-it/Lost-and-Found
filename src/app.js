3a833bae3523e6a5863f9244883cf72fe05695c9
tanmayy-2907 <tanmaynarkar2907@gmail.com>
2026-01-23 23:00:51 +0530

    fixed path error

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendDistPath));
 
   // Handle React routing (Return index.html for all other routes)
-  app.get("*", (req, res) => {
+  app.get("(.*)", (req, res) => {
     res.sendFile(path.resolve(frontendDistPath, "index.html"), (err) => {
       if (err) {
         console.error("❌ ERROR sending index.html:", err);
